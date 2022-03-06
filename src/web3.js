d5d2ad6452dbfb76006920c19e8e4558da307c9e
timothyAgevi <timothyagevi@gmail.com>
2022-03-06 16:04:43 +0300

    add web3.js

---
+++ src/web3.js
@@ -0,0 +1,6 @@
+import Web3 from 'web3';
+//copy of web 3 from web3 library ,currentProviser:metamaskprovider
+//Web3:localcopy of web3
+const web3= new Web3(window.currentProvider);
+
+export default web3;
