28fe58982d8e23f4d491ca89badd62a4812ce95f
timothyAgevi <timothyagevi@gmail.com>
2022-03-06 16:09:26 +0300

    delete class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React,{Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
 import web3 from './web3'
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
