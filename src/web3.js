10db9432d4932568afee40fdfc467c3a6e160ce1
timothyAgevi <timothyagevi@gmail.com>
2022-03-06 16:38:44 +0300

    implementation intent

--- src/web3.js
+++ src/web3.js
@@ -1,6 +1,6 @@
 import Web3 from 'web3';
 //copy of web 3 from web3 library ,currentProviser:metamaskprovider
 //Web3:localcopy of web3
-const web3= new Web3(window.currentProvider);
+const web3= new Web3(window.web3.currentProvider);
 
 export default web3;
